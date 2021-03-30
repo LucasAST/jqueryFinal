1cc300c4028142b95e8699f6959abc7b61b78263
LucasAST <freakfluk@gmail.com>
2021-03-30 18:51:04 -0300

    quase lá

--- public/js/app.js
+++ public/js/app.js
@@ -7,3 +7,22 @@
   $( function() {
     $( "#accordion" ).accordion();
   } );
+  //Carrosel
+  $(document).ready(function() {
+    $("#content-slider").lightSlider({
+              loop:true,
+              keyPress:true
+          });
+          $('#image-gallery').lightSlider({
+              gallery:true,
+              item:1,
+             
+              slideMargin: 0,
+              speed:500,
+              auto:true,
+              loop:true,
+              onSliderLoad: function() {
+                  $('#image-gallery').removeClass('cS-hidden');
+              }  
+          });
+  });
